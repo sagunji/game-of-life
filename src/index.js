bc0ac3a194fe053230ddba540a69a3a45a732a27
sagunji <karanjit.sagun01@gmail.com>
2019-04-09 18:24:14 +0545

    Create canvas as platform

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+// Box width
+var bw = 500;
+// Box height
+var bh = 500;
+// Padding
+// var p = 1;
 
-function StartGameOfLife() {
-
-    function CreateLife() {
-        life = new Life()
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+function drawBoard() {
+    for (var x = 0; x < bw; x += 50) {
+        for (var y = 0; y < bh; y += 50) {
+            context.strokeRect(x, y, 50, 50);
+        }
     }
 }
+
+drawBoard();
